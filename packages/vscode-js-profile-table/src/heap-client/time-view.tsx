156c61bbe65012bc58daa2cad746ed2445517be1
JuFeng Zhang <zjffun@gmail.com>
2022-01-25 17:37:06 +0800

    fix: unit

--- packages/vscode-js-profile-table/src/heap-client/time-view.tsx
+++ packages/vscode-js-profile-table/src/heap-client/time-view.tsx
@@ -145,11 +145,11 @@ const TimeViewRow: FunctionComponent<{
     >
       <div className={styles.duration} aria-labelledby="self-size-header">
         <ImpactBar impact={node.selfSize / node.totalSize} />
-        {decimalFormat.format(node.selfSize)}kB
+        {decimalFormat.format(node.selfSize / 1000)}kB
       </div>
       <div className={styles.duration} aria-labelledby="total-size-header">
         <ImpactBar impact={node.totalSize / root.totalSize} />
-        {decimalFormat.format(node.totalSize)}kB
+        {decimalFormat.format(node.totalSize / 1000)}kB
       </div>
       {!location ? (
         <div
